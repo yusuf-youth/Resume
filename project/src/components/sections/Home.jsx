704937ec69c29f940d16c9db6ac00e47b6513478
Yusuf <sds636adreno509@gmail.com>
2026-02-05 19:38:15 +0200

    Updated resume file.

--- project/src/components/sections/Home.jsx
+++ project/src/components/sections/Home.jsx
@@ -8,41 +8,41 @@ import Button from "../Button";
 import Section from "../Section";
 import resumeFile from "../../assets/Ramil_Bayramov_Frontend_Developer_Confidential.pdf";
 
-const Home = () => {
-  const { subheading, heading, text, buttonLabel } = useText(TEXTS.HOME);
+  const Home = () => {
+    const { subheading, heading, text, buttonLabel } = useText(TEXTS.HOME);
 
-  return (
-    <Section id="home" hasNoPadding={true}>
-      <div className="home">
-        <SwitchButton />
+    return (
+      <Section id="home" hasNoPadding={true}>
+        <div className="home">
+          <SwitchButton />
 
-        <div className="home__image-wrapper">
-          <img
-            className="home__image"
-            src={iconSrc}
-            alt="A profile photo"
-            width="200"
-            height="200"
-            loading="lazy"
-          />
-        </div>
-        <div className="home__content">
-          <h3 className="home__subtitle h5">{subheading}</h3>
-          <h2 className="home__title h1">{heading}</h2>
-          <p className="home__description">
-            {text} <br />
-          </p>
+          <div className="home__image-wrapper">
+            <img
+              className="home__image"
+              src={iconSrc}
+              alt="A profile photo"
+              width="200"
+              height="200"
+              loading="lazy"
+            />
+          </div>
+          <div className="home__content">
+            <h3 className="home__subtitle h5">{subheading}</h3>
+            <h2 className="home__title h1">{heading}</h2>
+            <p className="home__description">
+              {text} <br />
+            </p>
 
-          <div className="home__actions">
-            <Socials socials={SOCIALS} />
-            <Button className="home__button" href={resumeFile}>
-              {buttonLabel}
-            </Button>
+            <div className="home__actions">
+              <Socials socials={SOCIALS} />
+              <Button className="home__button" href={resumeFile}>
+                {buttonLabel}
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </Section>
-  );
-};
+      </Section>
+    );
+  };
 
 export default Home;
